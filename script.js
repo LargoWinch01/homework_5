8320a7e4f4c15b84de34a503861f9d4b32a7954a
LargoWinch01 <tokorobakidze7@gmail.com>
2024-09-04 17:08:18 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let temperature='input';
+
+if (temperature<0 && typeof temperature === 'number'){
+    console.log('frozen ');
+}
+else if (temperature>=0 && temperature <=10) {
+    console.log('very cold');
+}
+else if (temperature>=11 && temperature <=20) {
+    console.log('cold');
+}
+else if (temperature>=21 && temperature <=30) {
+    console.log('hot');
+}
+else if (temperature>30 ) {
+    console.log('very hot');
+}
+else{
+    console.log('error');
+}
